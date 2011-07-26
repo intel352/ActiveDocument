70a7ebf16c7e83f970a96ceccaf57672c6f85c08
kbrintle <none@none>
2011-07-26 16:01:34 -0400

    Fixing jon's shit

--- Form.php
+++ Form.php
@@ -2,28 +2,29 @@
 
 namespace ext\activedocument;
 
-use \CActiveForm;
+use \CActiveForm,
+    \CHtml;
 
 class Form extends CActiveForm {
-    
-	public function error($model,$attribute,$htmlOptions=array(),$enableAjaxValidation=true,$enableClientValidation=true) {
+
+    public function error($model, $attribute, $htmlOptions=array(), $enableAjaxValidation=true, $enableClientValidation=true) {
         /**
          * Determine input id
          */
-		$id=CHtml::activeId($model,$attribute);
-		$inputID=isset($htmlOptions['inputID']) ? $htmlOptions['inputID'] : $id;
-        
+        $id = CHtml::activeId($model, $attribute);
+        $inputID = isset($htmlOptions['inputID']) ? $htmlOptions['inputID'] : $id;
+
         /**
          * Let widget process as normal
          */
         $html = parent::error($model, $attribute, $htmlOptions, $enableAjaxValidation, $enableClientValidation);
-        
+
         /**
          * If inputid exists, update status field as necessary
          */
-		if($model instanceof Document && !$model->isNewRecord && isset($this->attributes[$inputID]))
-			$this->attributes[$inputID]['status']=1;
+        if ($model instanceof Document && !$model->isNewRecord && isset($this->attributes[$inputID]))
+            $this->attributes[$inputID]['status'] = 1;
         return $html;
     }
-    
+
 }
