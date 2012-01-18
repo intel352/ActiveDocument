f5b40a5c734b8a6da80c17f169b9d9e2b8fbc4c4
intel352 <none@none>
2012-01-18 03:16:58 -0500

    Updating methods to ensure compatibility with latest parent class changes

--- drivers/memory/Object.php
+++ drivers/memory/Object.php
@@ -4,65 +4,76 @@ namespace ext\activedocument\drivers\memory;
 
 /**
  * Object for Memory driver
- * 
+ *
  * @version $Version$
- * @author $Author$
+ * @author  $Author$
  */
 class Object extends \ext\activedocument\Object {
 
-    protected function loadObjectInstance($new=true) {
+    protected function loadObjectInstance($new = true) {
         if ($this->getKey() !== null && !$new)
             return $this->_container->getContainerInstance()->objects[$this->getKey()];
         return new \ArrayObject(array(), \ArrayObject::ARRAY_AS_PROPS);
     }
 
-    public function store() {
+    /**
+     * @return bool
+     */
+    protected function storeInternal() {
         $this->setObjectData($this->data);
-        if(parent::getKey()!==null)
+        if (parent::getKey() !== null)
             $this->_container->getContainerInstance()->objects[$this->getKey()] = $this->_objectInstance;
         else
             $this->_container->getContainerInstance()->objects[] = $this->_objectInstance;
         return true;
     }
 
-    public function delete() {
-        if($this->getKey()!==null)
+    /**
+     * @return bool
+     */
+    protected function deleteInternal() {
+        if ($this->getKey() !== null)
             unset($this->_container->getContainerInstance()->objects[$this->getKey()]);
         return true;
     }
 
-    public function reload() {
+    /**
+     * @return bool
+     */
+    protected function reloadInternal() {
         $this->_objectInstance = $this->loadObjectInstance(false);
         return true;
     }
-    
+
     public function setKey($key) {
-        if($key===$this->getKey())
+        if ($key === $this->getKey())
             return;
-        
-        if($key!==null)
+
+        if ($key !== null)
             $this->_container->getContainerInstance()->objects[$key] = $this->_objectInstance;
         else {
             $this->_container->getContainerInstance()->objects[] = $this->_objectInstance;
-            
-            $key = array_search($this->_objectInstance, (array) $this->_container->getContainerInstance()->objects, true);
+
+            $key = array_search($this->_objectInstance, (array)$this->_container->getContainerInstance()->objects, true);
         }
-        
+
         /**
          * Remove old key entry, if existed
          */
-        if($this->getKey()!==null)
+        if ($this->getKey() !== null)
             unset($this->_container->getContainerInstance()->objects[$this->getKey()]);
-        
+
         return parent::setKey($key);
     }
 
     protected function getObjectData() {
-        return (array) $this->_objectInstance;
+        return (array)$this->_objectInstance;
     }
 
+    /**
+     * @param mixed $data
+     */
     protected function setObjectData($data) {
-        $this->_objectInstance->exchangeArray(array_merge((array) $this->_objectInstance,(array) $data));
+        $this->_objectInstance->exchangeArray(array_merge((array)$this->_objectInstance, (array)$data));
     }
-
 }
